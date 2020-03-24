182afdce7f758f885fb3036d40755ccb69943c2d
Sh1ne <kosovchychserhiy@gmail.com>
2020-03-24 16:34:58 +0200

    Create project, do main window style

---
+++ WpfCalc/ViewModel/MainVM.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfCalc.ViewModel
+{
+    class MainVM : BindableBase
+    {
+        public MainVM()
+        {
+            
+        }
+    }
+}
