8c3289416e0bcad6ce16c64d7a05777eb4178be9
Sh1ne <kosovchychserhiy@gmail.com>
2020-03-25 12:10:05 +0200

    Fix btn focus style, add keydown handler

--- WpfCalc/ViewModel/MainVM.cs
+++ WpfCalc/ViewModel/MainVM.cs
@@ -1,18 +1,26 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace WpfCalc.ViewModel
 {
     class MainVM : BindableBase
     {
+        public DelegateCommand<KeyEventArgs> KeyDownEventCommand { get; private set; }
+
         public MainVM()
         {
-            
+            KeyDownEventCommand = new DelegateCommand<KeyEventArgs>(KeyDownEventHandler);
         }
+
+        public void KeyDownEventHandler(KeyEventArgs args)
+        {
+            
+        }        
     }
 }
