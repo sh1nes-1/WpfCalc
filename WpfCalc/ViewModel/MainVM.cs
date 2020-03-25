3f7880e586f43d867312ac306c722b05e58caa6d
Sh1ne <kosovchychserhiy@gmail.com>
2020-03-25 13:11:24 +0200

    Do resize of text in main TextBox

--- WpfCalc/ViewModel/MainVM.cs
+++ WpfCalc/ViewModel/MainVM.cs
@@ -10,17 +10,31 @@ using System.Windows.Input;
 namespace WpfCalc.ViewModel
 {
     class MainVM : BindableBase
-    {
+    {        
         public DelegateCommand<KeyEventArgs> KeyDownEventCommand { get; private set; }
+        public DelegateCommand<object> DigitPressEventCommand { get; private set; }
 
         public MainVM()
         {
             KeyDownEventCommand = new DelegateCommand<KeyEventArgs>(KeyDownEventHandler);
+            DigitPressEventCommand = new DelegateCommand<object>(DigitPressEventHandler);
         }
 
         public void KeyDownEventHandler(KeyEventArgs args)
         {
             
         }        
+
+        public void DigitPressEventHandler(object digit)
+        {
+            DisplayText += digit;
+        }
+
+        private string _displayText;
+        public string DisplayText
+        {
+            get => _displayText;
+            set => SetProperty(ref _displayText, value);
+        }
     }
 }
